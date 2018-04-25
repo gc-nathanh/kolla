8dca775d0eb697e11e756bba04856679ef82f395
Nick Jones <nick@dischord.org>
2018-04-25 11:22:09 +0100

    [WIP] Simplify backup script

--- docker/xtrabackup/backup.sh
+++ docker/xtrabackup/backup.sh
@@ -1,12 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 BACKUP_DIR=/backup/
 
-if [ -d $BACKUP_DIR/base ]; then
-	xtrabackup --backup --target-dir=$BACKUP_DIR/incremental \
-		--incremental-basedir=$BACKUP_DIR/base
-else
-	xtrabackup --backup --target-dir=$BACKUP_DIR/base
-	xtrabackup --prepare --target-dir=$BACKUP_DIR/base
-	xtrabackup --print-param --prepare --target-dir=$BACKUP_DIR/base
-fi
-
+xtrabackup --backup --target-dir=$BACKUP_DIR
+xtrabackup --prepare --target-dir=$BACKUP_DIR
+xtrabackup --print-param --prepare --target-dir=$BACKUP_DIR

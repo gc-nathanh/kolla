bdc9ba95c2e7a7ecd4b51360e0a80b2d49e68302
Nick Jones <nick@dischord.org>
2018-10-02 13:23:03 +0100

    [WIP] Yet another refactored backup script
    This script now takes two options (passed as an environment variable)
    and handles taking either an incremental or a full backup.
    
    Change-Id: Ib774278ba8cb2ec50b8c07ced35fddf36e3513f1

--- docker/xtrabackup/backup.sh
+++ docker/xtrabackup/backup.sh
@@ -1,6 +1,49 @@
 #!/usr/bin/env bash
-BACKUP_DIR=/backup/
 
-xtrabackup --backup --target-dir=$BACKUP_DIR
-xtrabackup --prepare --target-dir=$BACKUP_DIR
-xtrabackup --print-param --prepare --target-dir=$BACKUP_DIR
+set -eu
+
+# Execute a full backup
+backup_full() {
+    echo "Taking a full backup"
+    innobackupex --defaults-file=/etc/mysql/my.cnf \
+	--no-timestamp \
+    	--stream=xbstream \
+    	--compress \
+    	--history=$(date +%d-%m-%Y) ./ > \
+    	$BACKUP_DIR/mysqlbackup-$(date +%d-%m-%Y).xbc.xbs
+}
+
+# Execute an incremental backup
+backup_incremental() {
+    echo "Taking an incremental backup"
+    innobackupex --defaults-file=/etc/mysql/my.cnf \
+        --no-timestamp \
+        --stream=xbstream \
+        --compress \
+        --incremental \
+        --incremental-history-name=$(date +%d-%m-%Y) \
+        --history=$(date +%d-%m-%Y) ./ > \
+        incremental-$(date +%H)-mysqlbackup-$(date +%d-%m-%Y).qp.xbc.xbs
+}
+
+BACKUP_DIR=/backup/
+cd $BACKUP_DIR
+
+if [ -n $BACKUP_TYPE ]; then
+    case $BACKUP_TYPE in
+	"full")
+	backup_full
+	;;
+	"incremental")
+	backup_incremental
+	;;
+	*)
+	echo "Only full or incremental options are supported."
+	exit 1
+	;;
+    esac
+else
+    echo "You need to specify either full or incremental backup options."
+    exit 1
+fi
+
